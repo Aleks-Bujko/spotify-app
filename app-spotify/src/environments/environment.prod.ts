de9c66ae52554f6b704993a09943362bf303d7f5
Aleks Bujko <aleksander.bujko@unterschiedundmacher.com>
2021-03-24 17:07:04 +0100

    Removed keys from environment

--- app-spotify/src/environments/environment.prod.ts
+++ app-spotify/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  clientId: '8e66de17f54e4b80afeb07e8dcf71777',
-  clientSecret: '08fed5f8600643568fc33769fa8902a5',
+  clientId: 'xx',
+  clientSecret: 'xx',
   scope: 'user-read-private user-read-email user-modify-playback-state',
   redirectUri: 'http://localhost:4200/home/'
 };
